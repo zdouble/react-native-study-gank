d34b49048650aec14f7511f2deb14caa502a5550
zwg <339599487@qq.com>
2017-05-09 11:48:20 +0800

    update

--- index.android.js
+++ index.android.js
@@ -1,8 +1,7 @@
-
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import React, { Component } from 'react'
+import { AppRegistry } from 'react-native'
 import Home from './app/views/home'
 
 
 
-AppRegistry.registerComponent('gank', () => Home);
+AppRegistry.registerComponent('gank', () => Home)
